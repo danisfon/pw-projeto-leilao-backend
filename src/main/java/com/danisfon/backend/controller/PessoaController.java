94b5e298761224d8cc70203b422b318feddf2928
danisfon <danieledasilvafonseca@gmail.com>
2025-06-16 21:00:17 -0300

    add | controller

--- src/main/java/com/danisfon/backend/controller/PessoaController.java
+++ src/main/java/com/danisfon/backend/controller/PessoaController.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danisfon.backend.model.Pessoa;
 import com.danisfon.backend.service.PessoaService;
+
+import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -22,4 +29,21 @@ public class PessoaController {
     public ResponseEntity<List<Pessoa>> buscarTodos() {
         return ResponseEntity.ok(pessoaService.buscarTodos());
     }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> inserir(@Valid @RequestBody Pessoa pessoa) {
+        return ResponseEntity.ok(pessoaService.inserir(pessoa));
+    }
+
+    @PutMapping
+    public ResponseEntity<Pessoa> alterar(@Valid @RequestBody Pessoa pessoa) {
+        return ResponseEntity.ok(pessoaService.alterar(pessoa));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluir(@PathParam("id") Long id) {
+        pessoaService.excluir(id);
+        return ResponseEntity.ok("Excluindo");
+    }
+
 }
